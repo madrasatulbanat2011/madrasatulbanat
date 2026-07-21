43328a761d227d71c6fced5d0c39d8aa21f571ff
madrasatulbanat2011 <madrashatulbanat2011@gmail.com>
2026-07-21 21:43:32 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyD2q0mbXpWW2npF7twfmK2V93h766K_PIE",
+  authDomain: "barbakpur-madrasatul-banat.firebaseapp.com",
+  projectId: "barbakpur-madrasatul-banat",
+  storageBucket: "barbakpur-madrasatul-banat.firebasestorage.app",
+  messagingSenderId: "409129992513",
+  appId: "1:409129992513:web:db5b84a32c641e36dba1c0",
+  measurementId: "G-QWCVJP7MNG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
