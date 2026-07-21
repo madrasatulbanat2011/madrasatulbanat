53089a9752559a6253630d8fdbaa1079bb1cf0c6
madrasatulbanat2011 <madrashatulbanat2011@gmail.com>
2026-07-21 21:57:02 +0530

    Update admission.js

--- admission.js
+++ admission.js
@@ -1,1 +1,49 @@
+import { db } from "./firebase.js";
 
+import {
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const form = document.getElementById("admissionForm");
+
+form.addEventListener("submit", async (e) => {
+
+  e.preventDefault();
+
+  const formData = new FormData(form);
+
+  const student = {
+    studentName: formData.get("studentName"),
+    dob: formData.get("dob"),
+    class: formData.get("class"),
+    religion: formData.get("religion"),
+    fatherName: formData.get("fatherName"),
+    motherName: formData.get("motherName"),
+    mobile: formData.get("mobile"),
+    whatsapp: formData.get("whatsapp"),
+    village: formData.get("village"),
+    postOffice: formData.get("postOffice"),
+    policeStation: formData.get("policeStation"),
+    district: formData.get("district"),
+    pinCode: formData.get("pinCode"),
+    previousSchool: formData.get("previousSchool"),
+    remarks: formData.get("remarks")
+  };  
+  try {
+
+    await addDoc(collection(db, "students"), student);
+
+    alert("Admission Successful!");
+
+    form.reset();
+
+  } catch (error) {
+
+    console.error(error);
+
+    alert("Data Save Failed!");
+
+  }
+
+});
